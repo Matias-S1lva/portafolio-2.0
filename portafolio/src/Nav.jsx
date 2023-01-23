546977a9f17bcad629bc56a903935f1c1bdd4b6e
Matias-S1lva <matiasleandro752@gmail.com>
2023-01-23 01:02:28 -0300

    se hizo responsive toda la pagina

--- portafolio/src/Nav.jsx
+++ portafolio/src/Nav.jsx
@@ -1,10 +1,28 @@
 import "./CSS/Nav.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars} from "@fortawesome/free-solid-svg-icons";
+
 const Nav = () => {
+  
   return (
     <nav className="navbar">
+      
       <ul className="navbar-left">
         <li>
           <a href="">Mi portafolio</a>
+        </li>
+      </ul>
+      <ul className="menu-responsive">
+        <li>
+        <FontAwesomeIcon icon={faBars}/>
+        <ul className="menu-dropdown">
+          <li className="menu-dropdown-items">
+            <a href="">proyectos</a>
+          </li>
+          <li className="menu-dropdown-items">
+            <a href="">sobre mi</a>
+          </li>
+        </ul>
         </li>
       </ul>
       <ul className="navbar-right">

faa1b6f6feb3baea72d719d692d50720f2c4a055
Matias-S1lva <matiasleandro752@gmail.com>
2023-01-31 16:29:25 -0300

    [UPDATE] se agrego componente menu para mobile

---
+++ portafolio/src/Menu.jsx
@@ -0,0 +1,18 @@
+const Menu = ({ ocultarMenu }) => {
+  return (
+    <ul className="menu-oculto">
+      <li onClick={ocultarMenu}><a href="" style={{ pointerEvents: "none"}} >cerrar X</a></li>
+      <li>
+        <a href="#sobre-mi" onClick={ocultarMenu}>sobre mi</a>
+      </li>
+      <li>
+        <a href="#proyectos" onClick={ocultarMenu}>proyectos</a>
+      </li>
+      <li>
+        <a href="#contacto" onClick={ocultarMenu}>contacto</a>
+      </li>
+    </ul>
+  );
+};
+
+export { Menu };

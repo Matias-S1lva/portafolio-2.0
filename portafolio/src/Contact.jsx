04094b1290e5a88843853003c466c882a663897b
Matias-S1lva <matiasleandro752@gmail.com>
2023-02-01 00:20:44 -0300

    [UPDATE]  se agrego mensaje de success cuando se envia correctamente el formulario

--- portafolio/src/Contact.jsx
+++ portafolio/src/Contact.jsx
@@ -1,6 +1,7 @@
 import "./CSS/Contact.css";
-import AOS from 'aos';
+import React,{useState} from 'react';
 import emailjs from "@emailjs/browser";
+import {Success} from './Success'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPhone,
@@ -8,8 +9,9 @@ import {
   faPaperPlane,
 } from "@fortawesome/free-solid-svg-icons";
 
-AOS.init()
+
 const Contact = () => {
+  const [sucess, setSucess] = useState(false) 
   const sendMail = (event) => {
     event.preventDefault();
     emailjs
@@ -19,15 +21,20 @@ const Contact = () => {
         event.target,
         "x_SH79tf84gmLhOAg"
       )
-      .then((response) => console.log(response))
+      .then((response) => { 
+        if(response.status === 200) setSucess(true) 
+        setTimeout(() => setSucess(false),3000)
+      })
       .catch((err) => console.error(err));
   };
 
   return (
     <section className="section-form" >
+      {sucess && <Success/>}
+     
         <div className="container-form" data-aos="fade-up">
-          <div className="container-contact">
-            <h1 id="contacto">Contacto</h1>
+          <div className="container-contact" id="contacto">
+            <h1 >Contacto</h1>
             <div className="container-contact-items">
               <p>
                 <FontAwesomeIcon icon={faPhone} />
